bd94574afbde44deb7a2630f3b78b9e69260c867
nitingoyal05 <nitingoyal9393@gmail.com>
2025-01-17 00:48:46 +0530

    Added my project

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; 
+
+
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! Your Express server is running.');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
