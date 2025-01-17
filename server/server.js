27190c62444205b824759a0a39a9d791e9939b00
nitingoyal05 <nitingoyal9393@gmail.com>
2025-01-18 00:35:42 +0530

    added route file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import path from "path";
+import { ENV_VARS } from './config/envVars.js';
+
+
+
 const express = require('express');
 const app = express();
 const PORT = 3000; 
+
+
+app.use('/api/v1/auth',require('./routes/auth.route.js'));
+
 
 
 app.use(express.json()); 
